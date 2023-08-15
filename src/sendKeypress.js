9af6d0436691129f5aa9366d2eba892c5761d4ac
Kai Shoya <shonari77@gmail.com>
2023-08-15 09:10:51 +0900

    appVersionが非推奨になっていたため、userAgentからOSを判断するよう修正した

--- src/sendKeypress.js
+++ src/sendKeypress.js
@@ -1,7 +1,7 @@
 // Determine if the OS being used is macOS or something else. If macOS send metakey+d, otherwise send ctrl+d
 
 var OSName = 'other';
-if (navigator.appVersion.indexOf('Mac') != -1) OSName = 'MacOS';
+if (navigator.userAgent.indexOf('Mac') != -1) OSName = 'MacOS';
 
 if (OSName == 'MacOS') {
   document.dispatchEvent(
