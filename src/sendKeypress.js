7e7e7dc6c9c0ae770fba8bd7b884b6cb267f095d
Kai Shoya <shonari77@gmail.com>
2023-09-06 19:43:24 +0900

    sendKeypress.jsリファクタリング ブロックスコープを追加した

--- src/sendKeypress.js
+++ src/sendKeypress.js
@@ -1,26 +1,27 @@
 // Determine if the OS being used is macOS or something else. If macOS send metakey+d, otherwise send ctrl+d
+{
+  let OSName = 'other'
+  if (navigator.userAgent.indexOf('Mac') != -1) OSName = 'MacOS'
 
-var OSName = 'other';
-if (navigator.userAgent.indexOf('Mac') != -1) OSName = 'MacOS';
-
-if (OSName == 'MacOS') {
-  document.dispatchEvent(
-    new KeyboardEvent('keydown', {
-      bubbles: true,
-      cancelable: true,
-      metaKey: true,
-       keyCode: 68,
-       code: 'KeyD'
-     })
- );
-} else {
-  document.dispatchEvent(
-    new KeyboardEvent('keydown', {
-      bubbles: true,
-      cancelable: true,
-      ctrlKey: true,
-      keyCode: 68,
-      code: 'KeyD'
-    })
-  );
+  if (OSName == 'MacOS') {
+    document.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        metaKey: true,
+        keyCode: 68,
+        code: 'KeyD'
+      })
+    )
+  } else {
+    document.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        bubbles: true,
+        cancelable: true,
+        ctrlKey: true,
+        keyCode: 68,
+        code: 'KeyD'
+      })
+    )
+  }
 }
